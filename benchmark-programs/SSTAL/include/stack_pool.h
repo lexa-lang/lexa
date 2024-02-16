7adf2d4d6d9dc18115de2fcbcdd0dec950b5e0ca
Cong Ma <cong.ma@uwaterloo.ca>
2024-02-16 15:00:50 -0500

    remove deadcode

--- benchmark-programs/SSTAL/include/stack_pool.h
+++ benchmark-programs/SSTAL/include/stack_pool.h
@@ -35,36 +35,6 @@ char* get_stack() {
 void free_stack(char* stack) {
     int index = ((intptr_t)stack - (intptr_t)buffer - 1) / STACK_SIZE;
     bitmap |= (1 << index);
-    // if (stack >= buffer && stack < buffer + (STACK_SIZE * PREALLOCATED_STACKS)) {
-    //     int index = ((intptr_t)stack - (intptr_t)buffer) / STACK_SIZE;
-    //     bitmap |= (1 << index);
-    // } else {
-    //     // NB: why -1? Think what should happen when an empty stack is freeed.
-    //     // Because an empty stack is +STACK_SIZE from the beginning of the buffer,
-    //     // If we don't -1, the stack pointer will be pointing to the start of the next stack.
-    //     free((void*)(((intptr_t)stack - 1) / STACK_SIZE * STACK_SIZE));
-    // }
-}
-
-void avx2_memcpy_totally_aligned(void* dest, const void* src, size_t len) {
-    // Cast to appropriate pointer types for alignment
-    char* dst8 = (char*)dest;
-    const char* src8 = (char*)src;
-
-    // Calculate how many 256-bit chunks we can copy directly
-    size_t avx2_chunks = len / 32;
-
-    // AVX2 Copy Loop: Process 32 bytes (256 bits) at a time
-    for (size_t i = 0; i < avx2_chunks; ++i) {
-        __m256i ymm_data = _mm256_load_si256((const __m256i*)(src8 + i * 32));
-        _mm256_store_si256((__m256i*)(dst8 + i * 32), ymm_data);
-    }
-
-    // Tail Copy: Handle remaining bytes (less than 32)
-    // size_t tail = len % 32;
-    // for (size_t i = 0; i < tail; ++i) {
-    //     dst8[avx2_chunks * 32 + i] = src8[avx2_chunks * 32 + i];
-    // }
 }
 
 // Does not work for an empty stack. But such situation should not happen.
