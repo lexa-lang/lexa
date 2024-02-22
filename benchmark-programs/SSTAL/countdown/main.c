4c2465115c66ce25c43c9f91e64feb71183a2b75
Cong Ma <cong.ma@uwaterloo.ca>
2024-02-22 11:37:42 -0500

    better countdown.c

--- benchmark-programs/SSTAL/countdown/main.c
+++ benchmark-programs/SSTAL/countdown/main.c
@@ -1,123 +1,50 @@
 #include <stdint.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <longjmp.h>
-
-typedef struct {
-  mp_jmpbuf_t *ctx_jb;
-  mp_jmpbuf_t *rsp_jb;
-} exchanger_t;
-
-typedef enum {
-    GENERAL = 0,
-    TAIL,
-    ABORT
-} behaviour_t;
-
-typedef struct {
-  const behaviour_t behavior;
-  const void *func;
-} handler_def_t;
-typedef struct {
-  const handler_def_t* const defs;
-  const intptr_t* env;
-  exchanger_t* exchanger;
-} handler_t;
-
-typedef struct node {
-    int64_t value;
-    struct node* next;
-} node;
+#include <defs.h>
 
 static intptr_t ret_val;
 
-#define xmalloc(size) ({                \
-    void *_ptr = malloc(size);          \
-    if (_ptr == NULL) {                 \
-        exit(EXIT_FAILURE);             \
-    }                                   \
-    _ptr;                               \
-})
-
-int64_t get(intptr_t env[1], int64_t _){
+int64_t get(intptr_t *env, int64_t _){
     return *(int64_t*)env[0];
 }
 
-int64_t set(intptr_t env[1], int64_t n){
+int64_t set(intptr_t *env, int64_t n){
     *(int64_t*)env[0] = n;
-    return n;
+    return 0;
 }
 
-int countdown(handler_t* hdl_stub){
-    // Invoke get
-    int64_t arg = 0;
+// NOTE: declare functions with `static` sometimes helps the compiler to optimize the code
+static int64_t countdown(handler_t* hdl_stub){
     int64_t out;
-    const handler_def_t get_def = hdl_stub->defs[0];
-    const behaviour_t behaviour = get_def.behavior;
-    const void* func = get_def.func;
-    const intptr_t* env = hdl_stub->env;
-    exchanger_t* exc = hdl_stub->exchanger;
-    switch (behaviour) {
-        case GENERAL:
-            exc->rsp_jb = (mp_jmpbuf_t*)xmalloc(sizeof(mp_jmpbuf_t));
-            if (mp_setjmp(exc->rsp_jb) == 0) {
-                __asm__ (
-                    "movq %0, %%rsp\n\t"
-                    :: "r"(exc->ctx_jb->reg_sp)
-                );
-                ((void(*)(const intptr_t* const, exchanger_t*, int64_t))func)(env, exc, arg);
-                __builtin_unreachable();
-            } else {
-                out = (int64_t)ret_val;
-            }
-            break;
-        case TAIL:
-            out = ((int64_t(*)(const intptr_t* const, int64_t))func)(env, arg);
-            break;
-        case ABORT:
-            __asm__ (
-                "movq %0, %%rsp\n\t"
-                :: "r"(exc->ctx_jb->reg_sp)
-            );
-            ((void(*)(const intptr_t* const, exchanger_t*, int64_t))func)(env, exc, arg);
-            __builtin_unreachable();
-    }
-    if(out == 0){
-        return out;
+    RAISE(out, hdl_stub, 0, 0);
+    int64_t i = out;
+    if(i == 0){
+        return i;
     } else {
-        // Invoke set
-        int64_t arg = out - 1;
         int64_t out;
-        handler_def_t set_def = (handler_def_t)hdl_stub->defs[1];
-        switch (set_def.behavior) {
-            case GENERAL:
-                // TODO
-            case TAIL:
-                out = ((int64_t(*)(const intptr_t* const, int64_t))set_def.func)(hdl_stub->env, arg);
-                break;
-            case ABORT:
-                __asm__ (
-                    "movq %0, %%rsp\n\t"
-                    :: "r"(hdl_stub->exchanger->ctx_jb->reg_sp)
-                );
-                ((void(*)(const intptr_t* const, exchanger_t*, int64_t))set_def.func)(hdl_stub->env, hdl_stub->exchanger, arg);
-                __builtin_unreachable();
-        };
-        // Recurse
-        return countdown(hdl_stub);
+        RAISE(out, hdl_stub, 1, i - 1);
+        // NOTE: due to the presence of `setjmp` in the current function, the compiler refuses to do tail call optimization
+        //     so we need to use the `musttail` attribute to force the tail call optimization
+        __attribute__((musttail)) return countdown(hdl_stub); 
     }
 }
 
-int64_t run(int64_t n){
+// NOTE: declare functions with `static` sometimes helps the compiler to optimize the code
+static int64_t run(int64_t n){
     // Heap-allocate a reference cell
-    int64_t* a = (int64_t*)xmalloc(1 * sizeof(int64_t));
-    *a = n;
+    // NOTE: let's always heap-allocate the reference cells to align with the formalization.
+    //     We can optimize this later if we want to.
+    int64_t* s = (int64_t*)xmalloc(1 * sizeof(int64_t));
+    *s = n;
 
+    // NOTE: let's always allocate handler stubs on the stack. Only in very rare cases we need to allocate it on the heap,
+    //     and let's not worry about that for now.
     // stack allocate the handles' definitions
     handler_def_t hdl_defs[2] = {{TAIL, (void*)get}, {TAIL, (void*)set}};
     // stack allocate the handler and handle body's environment
-    intptr_t hdl_env[1] = {(intptr_t)a};
+    intptr_t hdl_env[1] = {(intptr_t)s};
     // stack allocate the handler struct
+    // NOTE: the `exchanger` field is set to `NULL` because all handlers we have are tail-recursive
     handler_t *hdl_stub = &(handler_t){.defs = hdl_defs, .env = hdl_env, .exchanger = NULL};
     // run the handle body
     int64_t out = countdown(hdl_stub);
@@ -125,7 +52,7 @@ int64_t run(int64_t n){
 }
 
 int main(int argc, char *argv[]){
-    int out = run(atoi(argv[1]));
-    printf("%d\n", out);
+    int64_t out = run(atoi(argv[1]));
+    printf("%ld\n", out);
     return 0;
 }
---
+++ benchmark-programs/SSTAL/include/defs.h
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <longjmp.h>
+
+typedef struct {
+  mp_jmpbuf_t *ctx_jb;
+  mp_jmpbuf_t *rsp_jb;
+} exchanger_t;
+
+typedef enum {
+    GENERAL = 0,
+    TAIL,
+    ABORT
+} behaviour_t;
+
+typedef struct {
+  const behaviour_t behavior;
+  const void *func;
+} handler_def_t;
+typedef struct {
+  const handler_def_t* const defs;
+  const intptr_t* env;
+  exchanger_t* exchanger;
+} handler_t;
+
+typedef struct node {
+    int64_t value;
+    struct node* next;
+} node;
+
+#define xmalloc(size) ({                \
+    void *_ptr = malloc(size);          \
+    if (_ptr == NULL) {                 \
+        exit(EXIT_FAILURE);             \
+    }                                   \
+    _ptr;                               \
+})
+
+typedef void(*HandlerFuncType)(const intptr_t* const, exchanger_t*, int64_t);
+typedef int64_t(*TailHandlerFuncType)(const intptr_t* const, int64_t);
+
+#define RAISE(out, stub, index, arg) \
+    switch (stub->defs[index].behavior) { \
+        case GENERAL: \
+            stub->exchanger->rsp_jb = (mp_jmpbuf_t*)xmalloc(sizeof(mp_jmpbuf_t)); \
+            if (mp_setjmp(stub->exchanger->rsp_jb) == 0) { \
+                __asm__ ( \
+                    "movq %0, %%rsp\n\t" \
+                    :: "r"(stub->exchanger->ctx_jb->reg_sp) \
+                ); \
+                ((HandlerFuncType)stub->defs[index].func)(stub->env, stub->exchanger, arg); \
+                __builtin_unreachable(); \
+            } else { \
+                out = (int64_t)ret_val; \
+            } \
+            break; \
+        case TAIL: \
+            out = ((TailHandlerFuncType)stub->defs[index].func)(stub->env, arg); \
+            break; \
+        case ABORT: \
+            __asm__ ( \
+                "movq %0, %%rsp\n\t" \
+                :: "r"(stub->exchanger->ctx_jb->reg_sp) \
+            ); \
+            ((HandlerFuncType)stub->defs[index].func)(stub->env, stub->exchanger, arg); \
+            __builtin_unreachable(); \
+    }
