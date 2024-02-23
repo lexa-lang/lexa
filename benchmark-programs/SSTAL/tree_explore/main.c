ec2b8b9d0294f1f2f7a375815913bf265aadee37
Cong Ma <cong.ma@uwaterloo.ca>
2024-02-22 20:43:35 -0500

    mechanized translation of tree_explore. not working yet

--- benchmark-programs/SSTAL/tree_explore/main.c
+++ benchmark-programs/SSTAL/tree_explore/main.c
@@ -3,226 +3,107 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <longjmp.h>
-#include <stack_pool.h>
-
-typedef struct {
-  mp_jmpbuf_t *ctx_jb;
-  mp_jmpbuf_t *rsp_jb;
-} exchanger_t;
-
-typedef struct {
-  void (*func)(const intptr_t* const, exchanger_t*, int64_t);
-  const intptr_t* env;
-  exchanger_t* exchanger;
-} handler_t;
-
-typedef struct node {
-    int64_t value;
-    struct node* next;
-} node;
-
-typedef struct tree {
-    int64_t value;
-    struct tree* left;
-    struct tree* right;
-} tree;
+#include <defs.h>
+#include <datastructure.h>
 
 static intptr_t ret_val;
 
-#define xmalloc(size) ({                \
-    void *_ptr = malloc(size);          \
-    if (_ptr == NULL) {                 \
-        exit(EXIT_FAILURE);             \
-    }                                   \
-    _ptr;                               \
-})
-
-node* append(node* xs1, node* xs2) {
-  if (xs1 == NULL) {
-    return xs2;
-  } else {
-    node* new_node = (node*)xmalloc(sizeof(node));
-    new_node->value = xs1->value;
-    new_node->next = append(xs1->next, xs2);
-    return new_node;
-  }
-}
-
-int64_t max(node* xs) {
-  if (xs == NULL) {
-    return 0;
-  } else {
-    int64_t m = max(xs->next);
-    if (xs->value > m) {
-      return xs->value;
-    } else {
-      return m;
-    }
-  }
-}
-
-tree* make(int64_t n) {
-  if (n == 0) {
-    return NULL;
-  } else {
-    tree* t = make(n - 1);
-    tree* newTree = (tree*)xmalloc(sizeof(tree));
+tree_t* make(int64_t n) {
+  return (n == 0) ? ({
+    treeLeaf();
+  }) : ({
+    tree_t* t = make(n - 1);
+    tree_t* newTree = (tree_t*)xmalloc(sizeof(tree_t));
     newTree->value = n;
     newTree->left = t;
     newTree->right = t;
-    return newTree;
-  }
+    newTree;
+  });
 }
 
 int64_t operator(int64_t x, int64_t y) {
-  return labs(x - (503 * y) + 37) % 1009;
+  return mathAbs(x - (503 * y) + 37) % 1009;
 }
 
 void choose(const intptr_t* self_env, exchanger_t* exc, int64_t _) {
   mp_jmpbuf_t* ctx_jb = exc->ctx_jb;
   mp_jmpbuf_t* rsp_jb = exc->rsp_jb;
-  void* rsp_jb_sp = rsp_jb->reg_sp;
+  void* rsp_sp = rsp_jb->reg_sp;
 
-  node* result1, *result2;
-  mp_jmpbuf_t my_jb;
-  exc->ctx_jb = &my_jb;
-  char* new_sp = dup_stack((char*)rsp_jb_sp);
-  rsp_jb->reg_sp = (void*)new_sp;
-  ret_val = true;
-  if (mp_setjmp(exc->ctx_jb) == 0) {
-    mp_longjmp(rsp_jb);
-    __builtin_unreachable();
-  } else {
-    result1 = (node*)ret_val;
-  }
-  free_stack(new_sp);
-
-  mp_jmpbuf_t my_jb2;
-  exc->ctx_jb = &my_jb2;
-  rsp_jb->reg_sp = rsp_jb_sp;
-  ret_val = false;
-  if (mp_setjmp(exc->ctx_jb) == 0) {
-    mp_longjmp(rsp_jb);
-    __builtin_unreachable();
-  } else {
-    result2 = (node*)ret_val;
-  }
-  free(rsp_jb);
-
-  node* result = append(result1, result2);
+  node_t* result = listAppend((node_t*)THROW(rsp_jb, rsp_sp, exc, true), 
+                              (node_t*)FINAL_THROW(rsp_jb, exc, false));
   ret_val = (intptr_t)result;
 
   mp_longjmp(ctx_jb);
 }
 
-int64_t explore(const intptr_t* self_env, handler_t* choose_stub, tree* t) {
-  int64_t* state = (int64_t*)self_env[0];
-  if (t == NULL) {
-    return *state;
-  } else {
-    bool decision;
-    // Invoke choose
-    mp_jmpbuf_t* rsp_jb = (mp_jmpbuf_t*)xmalloc(sizeof(mp_jmpbuf_t));
-    choose_stub->exchanger->rsp_jb = rsp_jb;
-    if (mp_setjmp(choose_stub->exchanger->rsp_jb) == 0) {
-      __asm__ (
-        "movq %0, %%rsp\n\t"
-        :: "r"(choose_stub->exchanger->ctx_jb->reg_sp)
-      );
-      choose_stub->func(choose_stub->env, choose_stub->exchanger, 0);
-      __builtin_unreachable();
-    } else {
-      decision = (bool)ret_val;
-    }
-
-    tree* next;
-    if (decision) {
-      next = t->left;
-    } else {
-      next = t->right;
-    }
-    *state = operator(*state, t->value);
-    return operator(t->value, explore(self_env, choose_stub, next));
-  }
+int64_t explore(intptr_t* const state, tree_t* tree, handler_t* choose_stub) {
+  return (treeIsLeaf(tree)) ?
+    ({ 
+      state[0];
+    }) 
+    : ({
+      tree_t* next = ({
+        (RAISE(choose_stub, 0, 0)) ? ({
+          treeLeft(tree);
+        }) : ({
+          treeRight(tree);
+        });
+      });
+      state[0] = operator(state[0], treeValue(tree));
+      operator(treeValue(tree), explore(state, next, choose_stub));
+    });
 }
 
 void body(handler_t* choose_stub) {
-  int64_t (*explore_func)(const intptr_t* const, handler_t*, tree*) = (int64_t(*)(const intptr_t* const, handler_t*, tree*))choose_stub->env[1];
-  const intptr_t* explore_env = (const intptr_t*)choose_stub->env[2];
-
-  int64_t result = explore_func(explore_env, choose_stub, (tree*)choose_stub->env[0]);
-
-  node* n = (node*)xmalloc(sizeof(node));
-  n->value = result;
-  n->next = NULL;
-
-  ret_val = (intptr_t)n;
+  ret_val = (intptr_t)({
+    listNode(
+      #pragma clang diagnostic ignored "-Wint-conversion"
+      explore(choose_stub->env[0], choose_stub->env[1], choose_stub), 
+      #pragma clang diagnostic warning "-Wint-conversion"
+      listEnd());
+  });
 
   mp_longjmp(choose_stub->exchanger->ctx_jb);
   __builtin_unreachable();
 }
 
-node* paths(const intptr_t* self_env) {
-  mp_jmpbuf_t* ctx_jb = (mp_jmpbuf_t*)xmalloc(sizeof(mp_jmpbuf_t));
-
-  exchanger_t *exc = (exchanger_t*)xmalloc(sizeof(exchanger_t));
-  exc->ctx_jb = ctx_jb;
-  exc->rsp_jb = NULL;
-
-  intptr_t* choose_env = (intptr_t*)xmalloc(sizeof(intptr_t) * 3);
-  choose_env[0] = (intptr_t)self_env[0];
-  choose_env[1] = (intptr_t)self_env[1];
-  choose_env[2] = (intptr_t)self_env[2];
-  handler_t* choose_stub = (handler_t*)xmalloc(sizeof(handler_t));
-  choose_stub->func = choose;
-  choose_stub->env = choose_env;
-  choose_stub->exchanger = exc;
-
-  node* out;
-  char* new_stack = get_stack();
-  if (mp_setjmp(ctx_jb) == 0) {
-    __asm__(
-      "movq %0, %%rsp\n\t"
-      :: "r"(new_stack)
-    );
-    body(choose_stub);
-    __builtin_unreachable();
-  } else {
-    out = (node*)ret_val;
-  }
-
-  free_stack(new_stack);
+node_t* paths(intptr_t* const state, tree_t* tree) {
   
-  return out;
+
+  exchanger_t exc;
+
+  handler_def_t choose_defs[1] = {{MULTISHOT, (void*)choose}};
+  intptr_t choose_env[2] = {(intptr_t)state, (intptr_t)tree};
+  handler_t choose_closure = {choose_defs, choose_env, &exc};
+  handler_t* choose_stub = &choose_closure;
+
+  return (node_t*)HANDLE(choose_stub, body);
+}
+
+int64_t loop(intptr_t* const state, tree_t* tree, int64_t i) {
+  return (i == 0) ? ({
+    state[0];
+  }) : ({
+    state[0] = listMax(paths(state, tree));
+    loop(state, tree, i - 1);
+  });
 }
 
 int64_t run(int64_t n){
-  tree* t = make(n);
-  int64_t* state = (int64_t*)xmalloc(sizeof(int64_t));
-  *state = 0;
+  tree_t* tree = make(n);
 
-  intptr_t* explore_env = (intptr_t*)xmalloc(sizeof(intptr_t));
-  explore_env[0] = (intptr_t)state;
+  int64_t* state = (int64_t*)xmalloc(sizeof(int64_t) * 1);
+  state[0] = 0;
 
-  intptr_t* paths_env = (intptr_t*)xmalloc(sizeof(intptr_t) * 3);
-  paths_env[0] = (intptr_t)t;
-  paths_env[1] = (intptr_t)explore;
-  paths_env[2] = (intptr_t)explore_env;
-
-
-  for (int64_t i = 10; i != 0; i--) {
-    *state = max(paths(paths_env));
-  }
-
-
-  return *state;
+  int64_t out = loop(state, tree, 10);
+  return out;
 }
 
 int main(int argc, char *argv[]){
     init_stack_pool();
     int64_t out = run(atoi(argv[1]));
-    printf("%d\n", out);
+    printf("%ld\n", out);
     destroy_stack_pool();
     return 0;
 }
