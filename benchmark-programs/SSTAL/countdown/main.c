8a667aebd9a93bd3a2a5c5273fd113c9f1aca2f4
Cong Ma <cong.ma@uwaterloo.ca>
2024-02-26 16:47:13 -0500

    update countdown

--- benchmark-programs/SSTAL/countdown/main.c
+++ benchmark-programs/SSTAL/countdown/main.c
@@ -4,20 +4,20 @@
 
 intptr_t ret_val;
 
-int64_t get(intptr_t *env, int64_t _){
+intptr_t get(intptr_t *env, intptr_t _){
     intptr_t *s = (intptr_t*)env[0];
     return s[0];
 }
 
-int64_t set(intptr_t *env, int64_t n){
+intptr_t set(intptr_t *env, intptr_t n){
     intptr_t *s = (intptr_t*)env[0];
     s[0] = n;
     return 0;
 }
 
 // NOTE: declare functions with `static` sometimes helps the compiler to optimize the code
-static int64_t countdown(handler_t* state_stub){
-    int64_t i = RAISE(state_stub, 0, 0);
+static intptr_t countdown(handler_t* state_stub){
+    intptr_t i = RAISE(state_stub, 0, 0);
     if(i == 0){
         return i;
     } else {
@@ -28,16 +28,19 @@ static int64_t countdown(handler_t* state_stub){
     }
 }
 
+static intptr_t body(handler_t* state_stub) {
+    return countdown(state_stub);
+}
+
 // NOTE: declare functions with `static` sometimes helps the compiler to optimize the code
-static int64_t run(int64_t n){
+static intptr_t run(intptr_t n){
     // Heap-allocate a reference cell
     // NOTE: let's always heap-allocate the reference cells to align with the formalization.
     //     We can optimize this later if we want to.
-    int64_t* s = (int64_t*)xmalloc(1 * sizeof(int64_t));
-    *s = n;
+    intptr_t s = (intptr_t)xmalloc(1 * sizeof(intptr_t));
+    ((intptr_t*)s)[0] = n;
 
-    intptr_t env[1] = {(intptr_t)s};
-    return HANDLE_TWO(countdown, TAIL, (void*)get, TAIL, (void*)set, env);
+    return HANDLE_TWO(body, TAIL, (void*)get, TAIL, (void*)set, s);
 }
 
 int main(int argc, char *argv[]){
