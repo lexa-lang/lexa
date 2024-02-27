7e0b352d8c0882b797d9ed505b45d3d5e23bfda2
Cong Ma <cong.ma@uwaterloo.ca>
2024-02-27 11:58:59 -0500

    remove setjmp in defs.h

--- benchmark-programs/SSTAL/include/defs.h
+++ benchmark-programs/SSTAL/include/defs.h
@@ -39,7 +39,9 @@ typedef struct {
 })
 
 typedef void(*HandlerFuncType)(const intptr_t* const, int64_t, exchanger_t*);
+typedef void(*BodyFuncType)(handler_t*);
 typedef int64_t(*TailHandlerFuncType)(const intptr_t* const, int64_t);
+typedef int64_t(*TailBodyFuncType)(handler_t*);
 
 extern intptr_t ret_val;
 
@@ -73,47 +75,52 @@ extern intptr_t ret_val;
         :: "r"(sp) : "rsp" \
     )
 
+#define SAVE_CONTEXT(jb, cont) \
+    __asm__ ( \
+        "movq    %1,  0(%0)      \n\t" \
+        "movq    %%rbx,  8(%0)    \n\t" \
+        "leaq    (%%rsp), %1      \n\t" \
+        "movq    %1, 16(%0)    \n\t" \
+        "movq    %%rbp, 24(%0)    \n\t" \
+        "movq    %%r12, 32(%0)    \n\t" \
+        "movq    %%r13, 40(%0)    \n\t" \
+        "movq    %%r14, 48(%0)    \n\t" \
+        "movq    %%r15, 56(%0)    \n\t" \
+        :: "r" (jb), "r" (&&cont) \
+    )
+
 // This function is used in place of setjmp, and therefore avoids the setjmp's returns_twice attribute from
 // preventing optimizations. Being a setjmp-like function, we need to ensure that the caller-saved
 // registers are saved by the prologue, so we use the noinline attribute to prevent inlining.
 __attribute__((noinline))
-int64_t save_switch_and_run(mp_jmpbuf_t* jb, void* sp, HandlerFuncType func, const intptr_t* const env, int64_t arg, exchanger_t* exc) {
-    __asm__ (
-        "movq    %1,  0(%0)      \n\t"
-        "movq    %%rbx,  8(%0)    \n\t"
-        "leaq    (%%rsp), %1      \n\t"
-        "movq    %1, 16(%0)    \n\t"
-        "movq    %%rbp, 24(%0)    \n\t"
-        "movq    %%r12, 32(%0)    \n\t"
-        "movq    %%r13, 40(%0)    \n\t"
-        "movq    %%r14, 48(%0)    \n\t"
-        "movq    %%r15, 56(%0)    \n\t"
-        :: "r" (jb), "r" (&&cont)
-    );
-    sp = (void*)((uintptr_t)sp & ~((uintptr_t)0xF)); // Align sp down to the nearest 16-byte boundary
-    __asm__ (
-        "movq %0, %%rsp\n\t"
-        :: "r"(sp) : "rsp"
-    );
-    func(env, arg, exc);
+int64_t save_switch_and_run_handler(mp_jmpbuf_t* jb, void* sp, HandlerFuncType func, handler_t* stub, int64_t arg) {
+    SAVE_CONTEXT(jb, cont);
+    SWITCH_SP((uintptr_t)sp & ~((uintptr_t)0xF)); // Align sp down to the nearest 16-byte boundary
+    func(stub->env, arg, stub->exchanger);
+cont:
+    return ret_val;
+}
+
+__attribute__((noinline))
+int64_t save_switch_and_run_body(mp_jmpbuf_t* jb, void* sp, BodyFuncType func, handler_t* stub) {
+    SAVE_CONTEXT(jb, cont);
+    SWITCH_SP((uintptr_t)sp & ~((uintptr_t)0xF)); // Align sp down to the nearest 16-byte boundary
+    func(stub);
+cont:
+    return ret_val;
+}
+
+__attribute__((noinline))
+int64_t save_and_run_body(mp_jmpbuf_t* jb, BodyFuncType func, handler_t* stub) {
+    SAVE_CONTEXT(jb, cont);
+    func(stub);
 cont:
     return ret_val;
 }
 
 __attribute__((noinline))
 int64_t save_and_restore(mp_jmpbuf_t* jb1, mp_jmpbuf_t* jb2) {
-    __asm__ (
-        "movq    %1,  0(%0)      \n\t"
-        "movq    %%rbx,  8(%0)    \n\t"
-        "leaq    (%%rsp), %1      \n\t"
-        "movq    %1, 16(%0)    \n\t"
-        "movq    %%rbp, 24(%0)    \n\t"
-        "movq    %%r12, 32(%0)    \n\t"
-        "movq    %%r13, 40(%0)    \n\t"
-        "movq    %%r14, 48(%0)    \n\t"
-        "movq    %%r15, 56(%0)    \n\t"
-        :: "r" (jb1), "r" (&&cont)
-    );
+    SAVE_CONTEXT(jb1, cont);
     __asm__ (
         "movq  8(%0), %%rbx    \n\t"
         "movq 16(%0), %%rsp    \n\t"
@@ -136,7 +143,7 @@ cont:
         handler_def_t* defs = STACK_ALLOC_ARRAY(handler_def_t, {mode, (void*)func}); \
         intptr_t* env = STACK_ALLOC_ARRAY(intptr_t, __VA_ARGS__); \
         handler_t *stub = STACK_ALLOC_STRUCT(handler_t, defs, env, NULL); \
-        out = body(stub); \
+        out = ((TailBodyFuncType)body)(stub); \
     } else { \
         if (mode == MULTISHOT || mode == SINGLESHOT) { \
             handler_def_t* defs = HEAP_ALLOC_ARRAY(handler_def_t, {mode, (void*)func}); \
@@ -145,11 +152,7 @@ cont:
             exchanger_t* exc = HEAP_ALLOC_STRUCT(exchanger_t, ctx_jb, NULL); \
             handler_t *stub = HEAP_ALLOC_STRUCT(handler_t, defs, env, exc); \
             char* new_sp = get_stack(); \
-            if (mp_setjmp(exc->ctx_jb) == 0) { \
-                SWITCH_SP(new_sp); \
-                body(stub); \
-                __builtin_unreachable(); \
-            } \
+            out = save_switch_and_run_body(ctx_jb, new_sp, (BodyFuncType)body, stub); \
             free_stack(new_sp); \
         } else { \
             handler_def_t* defs = STACK_ALLOC_ARRAY(handler_def_t, {mode, (void*)func}); \
@@ -157,12 +160,8 @@ cont:
             mp_jmpbuf_t* ctx_jb = STACK_ALLOC_STRUCT(mp_jmpbuf_t); \
             exchanger_t* exc = STACK_ALLOC_STRUCT(exchanger_t, ctx_jb, NULL); \
             handler_t *stub = STACK_ALLOC_STRUCT(handler_t, defs, env, exc); \
-            if (mp_setjmp(exc->ctx_jb) == 0) { \
-                body(stub); \
-                __builtin_unreachable(); \
-            } \
+            out = save_and_run_body(ctx_jb, (BodyFuncType)body, stub); \
         } \
-        out = (intptr_t)ret_val; \
     } \
     out; \
     })
@@ -183,11 +182,7 @@ cont:
             exchanger_t* exc = HEAP_ALLOC_STRUCT(exchanger_t, ctx_jb, NULL); \
             handler_t *stub = HEAP_ALLOC_STRUCT(handler_t, defs, env, exc); \
             char* new_sp = get_stack(); \
-            if (mp_setjmp(exc->ctx_jb) == 0) { \
-                SWITCH_SP(new_sp); \
-                body(stub); \
-                __builtin_unreachable(); \
-            } \
+            out = save_switch_and_run_body(ctx_jb, new_sp, (BodyFuncType)body, stub); \
             free_stack(new_sp); \
         } else { \
             handler_def_t* defs = STACK_ALLOC_ARRAY(handler_def_t, {mode1, (void*)func1}, {mode2, (void*)func2}); \
@@ -195,12 +190,8 @@ cont:
             mp_jmpbuf_t* ctx_jb = STACK_ALLOC_STRUCT(mp_jmpbuf_t); \
             exchanger_t* exc = STACK_ALLOC_STRUCT(exchanger_t, ctx_jb, NULL); \
             handler_t *stub = STACK_ALLOC_STRUCT(handler_t, defs, env, exc); \
-            if (mp_setjmp(exc->ctx_jb) == 0) { \
-                body(stub); \
-                __builtin_unreachable(); \
-            } \
+            out = save_and_run_body(ctx_jb, (BodyFuncType)body, stub); \
         } \
-        out = (intptr_t)ret_val; \
     } \
     out; \
     })
@@ -212,7 +203,7 @@ cont:
         case SINGLESHOT: \
         case MULTISHOT: {\
             stub->exchanger->rsp_jb = (mp_jmpbuf_t*)xmalloc(sizeof(mp_jmpbuf_t)); \
-            out = save_switch_and_run(stub->exchanger->rsp_jb, stub->exchanger->ctx_jb->reg_sp, ((HandlerFuncType)stub->defs[index].func), stub->env, arg, stub->exchanger); \
+            out = save_switch_and_run_handler(stub->exchanger->rsp_jb, stub->exchanger->ctx_jb->reg_sp, ((HandlerFuncType)stub->defs[index].func), stub, arg); \
             break; \
         } \
         case TAIL: \
