179db06ea4b7a1b6a563b2be7112f8c0a6f04bd5
Cong Ma <cong.ma@uwaterloo.ca>
2024-03-07 21:21:51 -0500

    effekt_0_2_2

--- flake.nix
+++ flake.nix
@@ -160,6 +160,52 @@
             cp $tmp_file $out/bin/effekt.sh
           '';
         };
+        packages.effekt_0_2_2 = sbt.mkSbtDerivation.${system} {
+          pname = "effekt";
+          version = "v0.2.2";
+
+          depsSha256 = "sha256-PF+t+rbWYt9NOiWVO9B7Ey8/TGtnj9ZTZkiMoWodf6A=";
+
+          src = (pkgs.fetchFromGitHub {
+            owner = "effekt-lang";
+            repo = "effekt";
+            rev = "6f8973ae77e4962b67b3cb626142fab7430a6cd8";
+            sha256 = "sha256-UdKgq1R7IAtmUxAo/pM2kHQVGM/9pkT1weutEZHNfRM="; 
+            fetchSubmodules = true;
+            leaveDotGit = true;
+            deepClone = true;
+          }).overrideAttrs (_: { #https://github.com/NixOS/nixpkgs/issues/195117#issuecomment-1410398050
+            GIT_CONFIG_COUNT = 1;
+            GIT_CONFIG_KEY_0 = "url.https://github.com/.insteadOf";
+            GIT_CONFIG_VALUE_0 = "git@github.com:";
+          });
+          overrideDepsAttrs = final: prev: {
+            preBuild = ''
+              export LANG=C.UTF-8
+            '';
+          };
+
+          propagatedBuildInputs = with pkgs; [ 
+            jre
+          ];
+          buildInputs = with pkgs; [
+            nodejs
+          ];
+
+          tmp_file = pkgs.writeText "effekt.sh" ''
+            #!/usr/bin/env bash
+            export SCRIPT_DIR=$(dirname $0)
+            java -jar "$SCRIPT_DIR/effekt" $@
+          '';
+          installPhase = ''
+            export LANG=C.UTF-8
+            export HOME=$out/home # make npm happy
+            mkdir -p $out
+            npm config set prefix $out
+            sbt install
+            cp $tmp_file $out/bin/effekt.sh
+          '';
+        };
         # Define the devShell for your project
         devShell = with pkgs; mkShell {
           nativeBuildInputs = [
@@ -168,7 +214,7 @@
           ];
           buildInputs = [
             mlton
-            self.packages.${system}.effekt
+            self.packages.${system}.effekt_0_2_2
             
             cmake
             ninja
