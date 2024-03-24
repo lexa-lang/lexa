84dbc9b3e3a05bb586ce399d692b9e57b9467b3e
Cong Ma <cong.ma@uwaterloo.ca>
2024-03-23 22:40:06 -0400

    effekt hopefully more reproducible

--- flake.nix
+++ flake.nix
@@ -81,7 +81,7 @@
           passthru.isClang = true;
         });
       in {
-        # packages.effekt = sbt.mkSbtDerivation.${system} {
+        # packages.effekt72f006 = sbt.mkSbtDerivation.${system} {
         #   pname = "effekt";
         #   version = "OOPSLA23";
 
@@ -91,10 +91,8 @@
         #     owner = "effekt-lang";
         #     repo = "effekt";
         #     rev = "72f0064f105d79a44e4593c63cfc9bebd84babf9";
-        #     sha256 = "sha256-mO1bAOiGCMJxUDEvedfLSlQnwQ+B8WGk1nLAEjvBZW4="; 
+        #     sha256 = "sha256-GNK+vfYx1crhi/Y8nj00ODjfnvmUNKBg9DT8R2xyD3s="; 
         #     fetchSubmodules = true;
-        #     leaveDotGit = true;
-        #     deepClone = true;
         #   }).overrideAttrs (_: { #https://github.com/NixOS/nixpkgs/issues/195117#issuecomment-1410398050
         #     GIT_CONFIG_COUNT = 1;
         #     GIT_CONFIG_KEY_0 = "url.https://github.com/.insteadOf";
@@ -168,52 +166,51 @@
 
           passthru.isClang = true;  
         });
-        packages.effekt_0_2_2 = sbt.mkSbtDerivation.${system} {
-          pname = "effekt";
-          version = "v0.2.2";
+        # packages.effekt_0_2_2 = sbt.mkSbtDerivation.${system} {
+        #   pname = "effekt";
+        #   version = "v0.2.2";
 
-          depsSha256 = "sha256-PF+t+rbWYt9NOiWVO9B7Ey8/TGtnj9ZTZkiMoWodf6A=";
+        #   depsSha256 = "sha256-PF+t+rbWYt9NOiWVO9B7Ey8/TGtnj9ZTZkiMoWodf6A=";
 
-          src = (pkgs.fetchFromGitHub {
-            owner = "effekt-lang";
-            repo = "effekt";
-            rev = "6f8973ae77e4962b67b3cb626142fab7430a6cd8";
-            sha256 = "sha256-6k8cnsJqJM4E4l/bA68Uopm3EZCwGM17XpADtwlJBSQ="; 
-            fetchSubmodules = true;
-            leaveDotGit = true;
-            deepClone = true;
-          }).overrideAttrs (_: { #https://github.com/NixOS/nixpkgs/issues/195117#issuecomment-1410398050
-            GIT_CONFIG_COUNT = 1;
-            GIT_CONFIG_KEY_0 = "url.https://github.com/.insteadOf";
-            GIT_CONFIG_VALUE_0 = "git@github.com:";
-          });
-          overrideDepsAttrs = final: prev: {
-            preBuild = ''
-              export LANG=C.UTF-8
-            '';
-          };
+        #   src = (pkgs.fetchFromGitHub {
+        #     owner = "effekt-lang";
+        #     repo = "effekt";
+        #     rev = "6f8973ae77e4962b67b3cb626142fab7430a6cd8";
+        #     sha256 = "sha256-1VrzzVxy8Zv4M8g2VKLKELXzofAGdKRKoy8ZQV3nNAw="; 
+        #     fetchSubmodules = true;
+        #     # leaveDotGit = true;
+        #   }).overrideAttrs (_: { #https://github.com/NixOS/nixpkgs/issues/195117#issuecomment-1410398050
+        #     GIT_CONFIG_COUNT = 1;
+        #     GIT_CONFIG_KEY_0 = "url.https://github.com/.insteadOf";
+        #     GIT_CONFIG_VALUE_0 = "git@github.com:";
+        #   });
+        #   overrideDepsAttrs = final: prev: {
+        #     preBuild = ''
+        #       export LANG=C.UTF-8
+        #     '';
+        #   };
 
-          propagatedBuildInputs = with pkgs; [ 
-            jre
-          ];
-          buildInputs = with pkgs; [
-            nodejs
-          ];
+        #   propagatedBuildInputs = with pkgs; [ 
+        #     jre
+        #   ];
+        #   buildInputs = with pkgs; [
+        #     nodejs
+        #   ];
 
-          tmp_file = pkgs.writeText "effekt.sh" ''
-            #!/usr/bin/env bash
-            export SCRIPT_DIR=$(dirname $0)
-            java -jar "$SCRIPT_DIR/effekt" $@
-          '';
-          installPhase = ''
-            export LANG=C.UTF-8
-            export HOME=$out/home # make npm happy
-            mkdir -p $out
-            npm config set prefix $out
-            sbt install
-            cp $tmp_file $out/bin/effekt.sh
-          '';
-        };
+        #   tmp_file = pkgs.writeText "effekt.sh" ''
+        #     #!/usr/bin/env bash
+        #     export SCRIPT_DIR=$(dirname $0)
+        #     java -jar "$SCRIPT_DIR/effekt" $@
+        #   '';
+        #   installPhase = ''
+        #     export LANG=C.UTF-8
+        #     export HOME=$out/home # make npm happy
+        #     mkdir -p $out
+        #     npm config set prefix $out
+        #     sbt install
+        #     cp $tmp_file $out/bin/effekt.sh
+        #   '';
+        # };
         # Define the devShell for your project
         devShell = with pkgs; mkShell {
           nativeBuildInputs = [
@@ -223,7 +220,8 @@
           ];
           buildInputs = [
             mlton
-            self.packages.${system}.effekt_0_2_2
+            # self.packages.${system}.effekt72f006
+            # self.packages.${system}.effekt_0_2_2
             
             cmake
             ninja
