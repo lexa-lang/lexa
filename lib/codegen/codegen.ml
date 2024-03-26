894f4cc70c5b829c360ed512bac12a4925825538
Zhaoyi 'August' Ge <z33ge@uwaterloo.ca>
2024-03-25 21:20:50 -0400

    escapeness analysis

--- lib/codegen/codegen.ml
+++ lib/codegen/codegen.ml
@@ -1,12 +1,15 @@
 open Syntax
 open Printf
 
+type handler_type = hdl_anno * bool
 type eff_sig_env = (var * string list) list
-type eff_type_env = (var * hdl_anno) list
+type eff_type_env = (var * handler_type) list
 type env = eff_sig_env * eff_type_env
+
 exception UndefinedHandler of string
 exception UndefinedSignature of string
 exception ParameterMismatch of string
+exception NestedFunction of string
 
 let get_eff_sig_env ((a, _) : env) = a
 let get_eff_type_env ((_, b) : env) = b
@@ -16,10 +19,10 @@ let lookup_hdl_type (hdl_var : var) (env : eff_type_env) : string =
   | None -> raise (UndefinedHandler hdl_var)
   | Some (_, hdl_type) -> 
       (match hdl_type with
-      | HDef -> "TAIL"
-      | HExc -> "ABORT"
-      | HHdl1 -> "SINGLESHOT"
-      | HHdls -> "MULTISHOT")
+      | (HDef, _) -> "TAIL"
+      | (HExc, _) -> "ABORT"
+      | (HHdl1, escapeness) -> "SINGLESHOT" ^ (if escapeness then " | ESCAPE_K" else "")
+      | (HHdls, escapeness) -> "MULTISHOT" ^ (if escapeness then " | ESCAPE_K" else ""))
       
 let lookup_hdl_index (hdl_var : var) (env : eff_sig_env) : int =
   match (List.find_opt (fun (_, dcls) -> List.mem hdl_var dcls) env) with
@@ -140,12 +143,48 @@ let rec sig_pass toplevel : eff_sig_env =
       (sig_name, dcl_list) :: (sig_pass tail)
   | _ :: tail -> sig_pass tail
 
+(* Given a handler body, determine if k escape *)
+let if_escape (h : hdl) : bool =
+  let (_, _, params, body) = h in
+  let rec get_resumption_var l = (match l with
+    | [] -> raise (ParameterMismatch "")
+    | x :: [] -> x
+    | _ :: tail -> get_resumption_var tail)
+  in
+  let resumption_var = get_resumption_var params in
+  let rec occurs_in_v (v : value) : bool = (match v with
+    | VVar x -> if (x = resumption_var) then true else false
+    | VAbs (name, _, _) -> raise (NestedFunction name)
+    | VInt _ -> false
+    | VBool _ -> false
+    | VEffSig (name, _) -> raise (NestedFunction name)
+    | VObj (name, _, _) -> raise (NestedFunction name))
+  and occurs_in_t (t : term) : bool = (match t with
+    | TValue v -> occurs_in_v v
+    | TArith (v1, _, v2) -> occurs_in_v v1 || occurs_in_v v2
+    | TCmp (v1, _, v2) -> occurs_in_v v1 || occurs_in_v v2
+    | TLet (_, t1, t2) -> occurs_in_t t1 || occurs_in_t t2
+    | TIf (cond, t1, t2) -> occurs_in_v cond || occurs_in_t t1 || occurs_in_t t2
+    | TApp (_, vs) -> List.exists (fun x -> occurs_in_v x) vs
+    | TNew vs -> List.exists (fun x -> occurs_in_v x) vs
+    | TGet (v, _) -> occurs_in_v v
+    | TSet (v1, _, v2) -> occurs_in_v v1 || occurs_in_v v2
+    | TRaise (_, _, vs) -> List.exists (fun x -> occurs_in_v x) vs
+    | TResume (_, _) -> false
+    | TResumeFinal (_, _) -> false
+    | THdl (envs, _, _, _) -> List.mem resumption_var envs)
+  in
+  occurs_in_t body
+
 (* Pass through the top levels keep track of effect types. *)
 let rec eff_type_pass toplevel : eff_type_env =
   match toplevel with
   | [] -> []
   | (VObj (_, _, hdl_list)) :: tail ->
-      List.map (fun (hdl_type, name, _, _) -> (name, hdl_type)) hdl_list @ (eff_type_pass tail)
+      (List.map (fun x -> 
+        let (hdl_anno, name, _, _) = x in
+        (name, (hdl_anno, if_escape x))) hdl_list)
+      @ (eff_type_pass tail)
   | _ :: tail -> eff_type_pass tail
 
 let genToplevel toplevel =
