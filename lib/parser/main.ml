50f0eda13684726af8092f9bedc5fa9b3b6aee3d
zhaoyig <z33ge@uwaterloo.ca>
2024-04-10 22:49:52 -0400

    parser error handling

--- lib/parser/main.ml
+++ lib/parser/main.ml
@@ -6,9 +6,36 @@ type location =
 
 exception Error of (location * string * string)
 
+let print_location loc ppf =
+  match loc with
+  | Nowhere ->
+      Format.fprintf ppf "unknown location"
+  | Location (begin_pos, end_pos) ->
+      let begin_char = begin_pos.Lexing.pos_cnum - begin_pos.Lexing.pos_bol in
+      let end_char = end_pos.Lexing.pos_cnum - begin_pos.Lexing.pos_bol in
+      let begin_line = begin_pos.Lexing.pos_lnum in
+      let filename = begin_pos.Lexing.pos_fname in
+
+      if String.length filename != 0 then
+        Format.fprintf ppf "file %S, line %d, charaters %d-%d" filename begin_line begin_char end_char
+      else
+        Format.fprintf ppf "line %d, characters %d-%d" (begin_line - 1) begin_char end_char
+
+let print_message ?(loc=Nowhere) msg_type =
+  match loc with
+  | Location _ ->
+     Format.eprintf "%s at %t:@\n" msg_type (print_location loc) ;
+     Format.kfprintf (fun ppf -> Format.fprintf ppf "@.") Format.err_formatter
+  | Nowhere ->
+     Format.eprintf "%s: " msg_type ;
+     Format.kfprintf (fun ppf -> Format.fprintf ppf "@.") Format.err_formatter
+
+let print_error (loc, err_type, msg) = print_message ~loc err_type "%s" msg
+
 let error ?(kind="Error") ?(loc=Nowhere) =
   let k _ =
     let msg = Format.flush_str_formatter () in
+      print_error (loc, kind, msg);
       raise (Error (loc, kind, msg))
   in
     Format.kfprintf k Format.str_formatter
@@ -29,32 +56,6 @@ let wrap_syntax_errors parser lex =
       syntax_error ~loc:(location_of_lex lex) "unrecognised symbol"
     | _ ->
       syntax_error ~loc:(location_of_lex lex) "syntax error"
-
-let print_location loc ppf =
-  match loc with
-  | Nowhere ->
-      Format.fprintf ppf "unknown location"
-  | Location (begin_pos, end_pos) ->
-      let begin_char = begin_pos.Lexing.pos_cnum - begin_pos.Lexing.pos_bol in
-      let end_char = end_pos.Lexing.pos_cnum - begin_pos.Lexing.pos_bol in
-      let begin_line = begin_pos.Lexing.pos_lnum in
-      let filename = begin_pos.Lexing.pos_fname in
-
-      if String.length filename != 0 then
-        Format.fprintf ppf "file %S, line %d, charaters %d-%d" filename begin_line begin_char end_char
-      else
-        Format.fprintf ppf "line %d, characters %d-%d" (begin_line - 1) begin_char end_char
-        
-let print_message ?(loc=Nowhere) msg_type =
-  match loc with
-  | Location _ ->
-     Format.eprintf "%s at %t:@\n" msg_type (print_location loc) ;
-     Format.kfprintf (fun ppf -> Format.fprintf ppf "@.") Format.err_formatter
-  | Nowhere ->
-     Format.eprintf "%s: " msg_type ;
-     Format.kfprintf (fun ppf -> Format.fprintf ppf "@.") Format.err_formatter
-
-let print_error (loc, err_type, msg) = print_message ~loc err_type "%s" msg
 
 let parse (s : string) : toplevel =
   let lexbuf = Lexing.from_string s in
